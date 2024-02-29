5d4dc44739dcf2863da295c82796d62b2ce1ab40
ScottBogumil <bogumil.scott@gmail.com>
2024-02-29 10:43:01 -0500

    added commenting

--- icon.sh
+++ icon.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-# creates an icon.desktop file with user input
+
+###############################################################################
+# this script takes user input, creates a file formatted for a desktop icon
+# and moves the file to the applicaitons folder to populate the icon
+###############################################################################
+
+
+# prompts user for input
 read -p "Enter filename with .desktop extension: " fileName
 read -p "Enter icon name: " iconName
 read -p "Enter version: " version
 read -p "Enter icon absolute path: " iconPath
 read -p "Enter executable file absolute path: " execFileLoc
 
+# creates file locally
 touch $fileName
+# writes user input to file
 cat <<EOM> $fileName
 [Desktop Entry]
 Version=$version
@@ -19,9 +28,13 @@ Name=$iconName
 Icon=$iconPath
 EOM
 
+# allows executable permissions
 sudo chmod 664 $fileName
+
+# changes ownership to root
 sudo chown root:root $fileName
 
+# moves file to correct folder
 sudo mv $fileName /usr/share/applications/$fileName
 
 echo "Icon created!"
